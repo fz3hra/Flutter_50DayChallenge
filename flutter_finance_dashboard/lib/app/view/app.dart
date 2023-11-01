6b271eee8129b467c91fe11240ab75d495f3afca
fz3hra <mujorefaatimah@gmail.com>
2023-11-01 10:48:00 +0400

    :zap: clean architecture

---
+++ flutter_finance_dashboard/lib/app/view/app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_finance_dashboard/counter/counter.dart';
+import 'package:flutter_finance_dashboard/l10n/l10n.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
+        colorScheme: ColorScheme.fromSwatch(
+          accentColor: const Color(0xFF13B9FF),
+        ),
+      ),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      home: const CounterPage(),
+    );
+  }
+}
---
+++ flutter_finance_dashboard/lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_finance_dashboard/app/app.dart';
+import 'package:flutter_finance_dashboard/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
